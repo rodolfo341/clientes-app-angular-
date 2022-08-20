2154a19de3def0ed8295daf8bda1f52e4a2bc6b1
Rodolfo Aravena <rodolfo.aravena.espinosa@gmail.com>
2022-08-19 21:38:45 -0400

    footer terminado y se agrega css css del footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+    selector: 'app-footer',
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    public autor: any = {nombre : "Rodolfo", apellido : "Aravena"};
+
+    ngOnInit(): void {
+    }
 
 }
