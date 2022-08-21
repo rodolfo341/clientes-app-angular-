62d39ed26cc4bb4b8738a72138ffdd88b1783645
Rodolfo Aravena <rodolfo.aravena.espinosa@gmail.com>
2022-08-21 16:42:12 -0400

    se agrega el ngFor

--- src/app/directiva/directiva.component.ts
+++ src/app/directiva/directiva.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-directiva',
-  templateUrl: './directiva.component.html',
-  styleUrls: ['./directiva.component.css']
+    selector: 'app-directiva',
+    templateUrl: './directiva.component.html',
+    styleUrls: ['./directiva.component.css']
 })
 export class DirectivaComponent implements OnInit {
+ 
+    listaCurso: string [] = ['TypeScript', 'JavaScript', 'Java SE', 'C#', 'PHP' ];
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
